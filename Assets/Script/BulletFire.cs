2b4637ae7f1b3575dc7096879730861f4e68d3fe
Peak <peakmama@gmail.com>
2022-07-22 20:58:28 +0700

    change to oculus

---
+++ Assets/Script/BulletFire.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class BulletFire : MonoBehaviour
+{
+    private GameObject BulletPrefeb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Shoot()
+    {
+        Vector3 ShootPos = transform.position;
+    }
+}
