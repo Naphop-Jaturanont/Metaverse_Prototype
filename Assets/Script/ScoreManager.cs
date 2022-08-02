91ea477bbb67533e696025dff84e79512a32e3ce
Peak <peakmama@gmail.com>
2022-08-02 18:05:21 +0700

    update

--- Assets/Script/ScoreManager.cs
+++ Assets/Script/ScoreManager.cs
@@ -7,8 +7,10 @@ public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
     public Text ScoreText;
+    public Text HightScore;
 
     private int Score = 0;
+    private int Hightscore = 0;
 
     private void Awake()
     {
@@ -17,12 +19,18 @@ public class ScoreManager : MonoBehaviour
 
     void Start()
     {
+        Hightscore = PlayerPrefs.GetInt("Hightscore", 0);
         ScoreText.text = Score.ToString() + " POINTS";
+        HightScore.text = Hightscore.ToString() + " POINTS";
     }
 
     public void AddPoint()
     {
         Score += 1;
         ScoreText.text = Score.ToString() + " POINTS";
+        if (Hightscore < Score)
+        {
+         PlayerPrefs.SetInt("Hightscore",Score);   
+        }
     }
 }
